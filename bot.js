ee8cbd16c9d80b7ba7191ef04f3e3f3d260edae8
IxRawan <46265613+IxRawan@users.noreply.github.com>
2019-01-02 03:12:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js")
 const client = new Discord.Client();
-var prefix = "AR!"
+var prefix = "AR"
 
 
 
@@ -9,39 +9,39 @@ client.on('message', function(message) {
     let args = message.content.split(" ").slice(1).join(" ");
     if(message.content.startsWith(prefix + "sn")) {
                 if(message.author.id !== myID) return;
-            if(!args) return message.reply('اكتبي الحالة الي تريديها يا اسطورة يا روان');
+            if(!args) return message.reply('اكتبي اي اسم ياسطورة يا روان.');
         client.user.setUsername(args);
-        message.channel.send('**تم يا اسطورة يا روان تغيرت اسمكي**').then(msg => {
+        message.channel.send('**SetName,Done!**').then(msg => {
            msg.delete(5000);
           message.delete(5000);
         });
     } else if(message.content.startsWith(prefix + "s")) {
                 if(message.author.id !== myID) return;
-            if(!args) return message.reply('اكتبي الحالة الي تريديها يا اسطورة يا روان');
+            if(!args) return message.reply('اكتبي اي حالة يا اسطورة يا روان.');
         client.user.setGame(args , 'https://twitch.tv/6xlez1');
-        message.channel.send('**تم يا اسطورة يا روان صرتي واتشنق**').then(msg => {
+        message.channel.send('**Streaming,Done!🎶**').then(msg => {
            msg.delete(5000);
           message.delete(5000);
         });
     } else if(message.content.startsWith(prefix + "p")) {
                         if(message.author.id !== myID) return;
-            if(!args) return message.reply('اكتبي الحالة الي تريديها يا اسطورة يا روان');
+            if(!args) return message.reply('اكتبي اي حالة يا اسطورة يا روان.');
         client.user.setGame(args);
-        message.channel.send('**تم يا اسطورة يا روان صرتي بلاينق**').then(msg => {
+        message.channel.send('**تم ياسطورة يا روان صرتي بلاينق**').then(msg => {
            msg.delete(5000);
           message.delete(5000);
         });
     } else if(message.content.startsWith(prefix + "l")) {
                         if(message.author.id !== myID) return;
-            if(!args) return message.reply('اكتبي الحالة الي تريديها يا اسطورة يا روان');
+            if(!args) return message.reply('اكتبي اي حالة يا اسطورة يا روان.');
         client.user.setActivity(args, {type:'LISTENING'});
-        message.channel.send('**تم يا اسطورة يا روان صرتي لستنق**').then(msg => {
+        message.channel.send('**تم ياسطورة يا روان صرتي لستنق**').then(msg => {
            msg.delete(5000);
           message.delete(5000);
         });
     } else if(message.content.startsWith(prefix + "w")) {
                         if(message.author.id !== myID) return;
-            if(!args) return message.reply('اكتبي الحالة الي تريديها يا اسطورة يا روان');
+            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
         client.user.setActivity(args, {type:'WATCHING'});
         message.channel.send('**تم يا اسطورة يا روان صرتي واتشنق**').then(msg => {
            msg.delete(5000);
@@ -50,8 +50,8 @@ client.on('message', function(message) {
     } else if(message.content.startsWith(prefix + "setavatar")) {
                         if(message.author.id !== myID) return;
         client.user.setAvatar(args);
-        message.channel.send('تم يا اسطورة يا روان تغيرت الصورة').then(msg => {
-                if(!args) return message.reply(اكتبي الحالة الي تريديها يا اسطورة يا روان');
+        message.channel.send(':white_check_mark: Done!').then(msg => {
+                if(!args) return message.reply('اكتبي الحالة الي تريديها ياسطورة يا روان.');
            msg.delete(5000);
           message.delete(5000);
         });
